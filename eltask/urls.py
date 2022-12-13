2740cb48e5149220e55badc38cd2d762000bc8ef
gibeongideon <43305107+gibeongideon@users.noreply.github.com>
2022-12-13 23:52:00 +0300

    updaete _static url

--- eltask/urls.py
+++ eltask/urls.py
@@ -24,3 +24,10 @@ urlpatterns = [
     path('', admin.site.urls),
     path("dashboard/", include("dashboard.urls")),
 ]
+
+from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+# Serve static and media files from development server
+urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
