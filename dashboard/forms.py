7eae9872fec77a1beadbccaa0b8efea3f35e5f00
gibeongideon <kipngeno.gibeon@gmail.com>
2023-01-13 02:59:45 +0300

    clean_user

--- dashboard/forms.py
+++ dashboard/forms.py
@@ -4,8 +4,8 @@ from .models import Job,Bid
 class JobForm(forms.ModelForm):
     class Meta:
         model = Job
-        exclude = (
-        "id"
+        exclude = (     
+        "id",
         "user",
         "assigned_to",
         "status",
