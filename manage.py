3aa7482e048f953b7eb7a97a408a393553c85367
BackendCoder2 <129889939+BackendCoder2@users.noreply.github.com>
2023-04-08 13:14:13 +0000

    change0

--- manage.py
+++ manage.py
@@ -1,12 +1,13 @@
-#!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
 import os
 import sys
 
+from django.utils.regex_helper import _lazy_re_compile
+import django.http.request    
+django.http.request.host_validation_re = _lazy_re_compile(r"[a-zA-z0-9.:]*")
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'eltask.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project01.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
