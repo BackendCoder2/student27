d6835f3fa33c01033f98ba94358e46c111ba0a43
BackendCoder2 <129889939+BackendCoder2@users.noreply.github.com>
2023-04-08 14:26:00 +0000

    kjkfjkgjf

--- eltask/settings.py
+++ eltask/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-12rqgs_jnznhncse+f_qb*x4jrvw)g%a26xbi74+kx77gmed=@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['157.230.17.135']
 
